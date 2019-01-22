de09def89f536dd6d310947378a9ea75b0cb0b89
SubramanyaRaj5 <46887502+SubramanyaRaj5@users.noreply.github.com>
2019-01-22 10:21:31 +0530

    Add files via upload

---
+++ s3ripting.py
@@ -0,0 +1,11 @@
+import boto3
+
+s3=boto3.resource('s3')
+bucket_name="BucketMRaj"
+try:
+    response=s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint':'us-east-2'})
+print(response)
+
+except Exception as error:
+print(error)
+
